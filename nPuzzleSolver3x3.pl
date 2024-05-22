084a6e497167374312c07c85a21ae0e2b838b108
miruna-iosub <iosub.miruna@student.uaic.ro>
2024-05-22 10:23:33 +0300

    final version for 3x3 solver

--- nPuzzleSolver3x3.pl
+++ nPuzzleSolver3x3.pl
@@ -43,40 +43,57 @@ check_order([1, 2, *, 3, 4, 5, 6, 7, 8]).
 check_order([1, *, 2, 3, 4, 5, 6, 7, 8]).
 check_order([*, 1, 2, 3, 4, 5, 6, 7, 8]).
 
-
 matches_order([], []).
 matches_order([H|T1], [H|T2]) :- matches_order(T1, T2).
 matches_order([*|T1], [_|T2]) :- matches_order(T1, T2).
 
-move(state(*, B, C, D, E, F, G, H, J), state(B, *, C, D, E, F, G, H, J), right).
-move(state(*, B, C, D, E, F, G, H, J), state(D, B, C, *, E, F, G, H, J), down).
+% Define possible moves for the blank space (*)
+move(State1, State2, Move) :-
+    state_to_list(State1, List),
+    nth0(Index, List, *),
+    valid_move(Index, NextIndex, Move),
+    swap(List, Index, NextIndex, NewList),
+    list_to_state(NewList, State2).
 
-move(state(A, *, C, D, E, F, G, H, J), state(*, A, C, D, E, F, G, H, J), left).
-move(state(A, *, C, D, E, F, G, H, J), state(A, C, *, D, E, F, G, H, J), right).
-move(state(A, *, C, D, E, F, G, H, J), state(A, E, C, D, *, F, G, H, J), down).
+% Convert state to list representation
+state_to_list(state(A, B, C, D, E, F, G, H, I), [A, B, C, D, E, F, G, H, I]).
 
-move(state(A, B, *, D, E, F, G, H, J), state(A, *, B, D, E, F, G, H, J), left).
-move(state(A, B, *, D, E, F, G, H, J), state(A, B, F, D, E, *, G, H, J), down).
+% Swap elements at positions Index and NextIndex in the list
+swap(List, Index, NextIndex, NewList) :-
+    nth0(Index, List, Elem1),
+    nth0(NextIndex, List, Elem2),
+    set_elem(List, Index, Elem2, TempList),
+    set_elem(TempList, NextIndex, Elem1, NewList).
 
-move(state(A, B, C, *, E, F, G, H, J), state(*, B, C, A, E, F, G, H, J), up).
-move(state(A, B, C, *, E, F, G, H, J), state(A, B, C, E, *, F, G, H, J), right).
-move(state(A, B, C, *, E, F, G, H, J), state(A, B, C, G, E, F, *, H, J), down).
+% Set element at specific position in the list
+set_elem([_|T], 0, Elem, [Elem|T]).
+set_elem([H|T], Index, Elem, [H|R]) :-
+    Index > 0,
+    Index1 is Index - 1,
+    set_elem(T, Index1, Elem, R).
 
-move(state(A, B, C, D, *, F, G, H, J), state(A, *, C, D, B, F, G, H, J), up).
-move(state(A, B, C, D, *, F, G, H, J), state(A, B, C, D, F, *, G, H, J), right).
-move(state(A, B, C, D, *, F, G, H, J), state(A, B, C, D, H, F, G, *, J), down).
-move(state(A, B, C, D, *, F, G, H, J), state(A, B, C, *, D, F, G, H, J), left).
-
-move(state(A, B, C, D, E, *, G, H, J), state(A, B, *, D, E, C, G, H, J), up).
-move(state(A, B, C, D, E, *, G, H, J), state(A, B, C, D, *, E, G, H, J), left).
-move(state(A, B, C, D, E, *, G, H, J), state(A, B, C, D, E, J, G, H, *), down).
-
-move(state(A, B, C, D, E, F, *, H, J), state(A, B, C, D, E, F, H, *, J), left).
-move(state(A, B, C, D, E, F, *, H, J), state(A, B, C, *, E, F, D, H, J), up).
-
-move(state(A, B, C, D, E, F, G, *, J), state(A, B, C, D, E, F, *, G, J), left).
-move(state(A, B, C, D, E, F, G, *, J), state(A, B, C, D, *, F, G, E, J), up).
-move(state(A, B, C, D, E, F, G, *, J), state(A, B, C, D, E, F, G, J, *), right).
-
-move(state(A, B, C, D, E, F, G, H, *), state(A, B, C, D, E, *, G, H, F), up).
-move(state(A, B, C, D, E, F, G, H, *), state(A, B, C, D, E, F, G, *, H), left).
+% Define valid moves for each position of the blank space
+valid_move(0, 1, right).
+valid_move(0, 3, down).
+valid_move(1, 0, left).
+valid_move(1, 2, right).
+valid_move(1, 4, down).
+valid_move(2, 1, left).
+valid_move(2, 5, down).
+valid_move(3, 0, up).
+valid_move(3, 4, right).
+valid_move(3, 6, down).
+valid_move(4, 1, up).
+valid_move(4, 3, left).
+valid_move(4, 5, right).
+valid_move(4, 7, down).
+valid_move(5, 2, up).
+valid_move(5, 4, left).
+valid_move(5, 8, down).
+valid_move(6, 3, up).
+valid_move(6, 7, right).
+valid_move(7, 4, up).
+valid_move(7, 6, left).
+valid_move(7, 8, right).
+valid_move(8, 5, up).
+valid_move(8, 7, left).
